7a7b8e47e259a4a6361188bdcfa6e03071df5014
tungbeooooi <grumps2007pun0suy@hotmail.com>
2024-05-11 21:09:05 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) minIndex = j;
-    }
-    if (minIndex !== i) {
-      let temp = arr[i];
-      arr[i] = arr[minIndex];
-      arr[minIndex] = temp;
-    }
+function rob(nums) {
+  let prevMax = 0;
+  let currMax = 0;
+  for (const num of nums) {
+    const temp = currMax;
+    currMax = Math.max(prevMax + num, currMax);
+    prevMax = temp;
   }
-  return arr;
+  return currMax;
 }
