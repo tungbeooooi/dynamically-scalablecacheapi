a85362a8854d3e71b3b255a22fef54cc7e90cfd2
tungbeooooi <grumps2007pun0suy@hotmail.com>
2024-05-14 23:23:44 +0700

    hello

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const factorial = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  let result = 1;
-  for (let i = 2; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-};
+function findKthLargest(nums, k) {
+  nums.sort((a, b) => b - a);
+  return nums[k - 1];
+}
