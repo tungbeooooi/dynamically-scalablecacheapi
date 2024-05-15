b4acfc2041955ec73c15f8249ed8759103fddb6b
tungbeooooi <grumps2007pun0suy@hotmail.com>
2024-05-15 19:08:01 +0700

    update task

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function findKthLargest(nums, k) {
-  nums.sort((a, b) => b - a);
-  return nums[k - 1];
+function allPathsSourceTarget(graph) {
+  const result = [];
+  const target = graph.length - 1;
+  dfs(graph, 0, [0]);
+  function dfs(graph, node, path) {
+    if (node === target) {
+      result.push([...path]);
+      return;
+    }
+    for (const neighbor of graph[node]) {
+      path.push(neighbor);
+      dfs(graph, neighbor, path);
+      path.pop();
+    }
+  }
+  return result;
 }
